697b21f7e6e63e3677e04215a77ef0dbc330c4bc
tannuverma <73612363+tannuverma@users.noreply.github.com>
2022-08-18 14:42:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function myfunc(){
+    var text1=document.getElementById('inputuser');
+    text1.select();
+    text1.setSelectionRange(0,1000);
+    console.log(text1)
+    navigator.clipboard.writeText(text1.value);
+}
+/*$(document).ready(function(){
+    $('#btn').click(function(){
+        $('#inputuser').select()
+        document.execCommand("copy");
+    })
+})*/
